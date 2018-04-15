967978228ec472c51b7e17e7c69ca8df1a88e273
Alexandr Kholodov <alexandr.khol@gmail.com>
2018-04-15 11:30:03 +0300

    1.3.1 Maven Plugins [#49546]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j;
 
+/**
+ * Class Calculate.
+ * @author Alexandr Kh.
+ * @since 15.04.2018
+ * @version 0.1
+ */
 public class Calculate {
-    
+    /**
+     * Main.
+     * @param args args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
