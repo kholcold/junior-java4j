d2626f87a9d49bec596dbb9e87ea47567000c11a
Alexandr Kholodov <alexandr.khol@gmail.com>
2018-09-18 20:25:25 +0300

    Крестики-нолики на JavaFX [#49777]

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -1,5 +1,14 @@
 package ru.job4j.tictactoe;
 
+/**
+ * Class Logic3T.
+ * В этом задании нужно реализовать игру крестики-нолики.
+ * В этом задании задан каркас приложения. Вам нужно реализовать только один класс Logic3T.
+ * Игра имеет визуальный интерфейс сделанных через JavaFX.
+ *
+ * @author Alexandr Kholodov (alexandr.khol@gmail.com).
+ * @since 18.06.2018.
+ */
 public class Logic3T {
     private final Figure3T[][] table;
 
@@ -7,15 +16,79 @@ public class Logic3T {
         this.table = table;
     }
 
+    /**
+     * Логика крестиков.
+     *
+     * @return результат.
+     */
     public boolean isWinnerX() {
-        return false;
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            if (table[0][0].hasMarkX()
+                    && table[i][i].hasMarkX()
+                    && table[table.length - 1][table.length - 1].hasMarkX()) {
+                result = true;
+            } else if (table[table.length - 1][i].hasMarkX()
+                    && table[table.length - 2][table.length - 2].hasMarkX()
+                    && table[i][table.length - 1].hasMarkX()) {
+                result = true;
+            } else if (table[i][0].hasMarkX()
+                    && table[i][1].hasMarkX()
+                    && table[i][2].hasMarkX()) {
+                result = true;
+            } else if (table[0][i].hasMarkX()
+                    && table[1][i].hasMarkX()
+                    && table[2][i].hasMarkX()) {
+                result = true;
+            }
+        }
+        return result;
     }
 
+    /**
+     * Логика ноликов.
+     *
+     * @return результат.
+     */
     public boolean isWinnerO() {
-        return false;
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            if (table[0][0].hasMarkO()
+                    && table[i][i].hasMarkO()
+                    && table[table.length - 1][table.length - 1].hasMarkO()) {
+                result = true;
+            } else if (table[table.length - 1][i].hasMarkO()
+                    && table[table.length - 2][table.length - 2].hasMarkO()
+                    && table[i][table.length - 1].hasMarkO()) {
+                result = true;
+            } else if (table[i][0].hasMarkO()
+                    && table[i][1].hasMarkO()
+                    && table[i][2].hasMarkO()) {
+                result = true;
+            } else if (table[0][i].hasMarkO()
+                    && table[1][i].hasMarkO()
+                    && table[2][i].hasMarkO()) {
+                result = true;
+            }
+        }
+        return result;
     }
 
+    /**
+     * Проверка на пустые клетки.
+     *
+     * @return результат.
+     */
     public boolean hasGap() {
-        return true;
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                if (!table[i][j].hasMarkX() && !table[i][j].hasMarkO()) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
+++ chapter_001/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
@@ -38,4 +38,37 @@ public class Logic3TTest {
         Logic3T login = new Logic3T(table);
         assertThat(login.hasGap(), is(true));
     }
+
+    @Test
+    public void whenHasXWinnerUserTest() {
+        Figure3T[][] table = {
+                {new Figure3T(true), new Figure3T(true), new Figure3T(true)},
+                {new Figure3T(false), new Figure3T(false), new Figure3T()},
+                {new Figure3T(), new Figure3T(), new Figure3T(true)},
+        };
+        Logic3T login = new Logic3T(table);
+        assertThat(login.isWinnerX(), is(true));
+    }
+
+    @Test
+    public void whenHasOWinnerUserTest() {
+        Figure3T[][] table = {
+                {new Figure3T(false), new Figure3T(false), new Figure3T(false)},
+                {new Figure3T(true), new Figure3T(true), new Figure3T()},
+                {new Figure3T(false), new Figure3T(), new Figure3T(true)},
+        };
+        Logic3T login = new Logic3T(table);
+        assertThat(login.isWinnerO(), is(true));
+    }
+
+    @Test
+    public void whenHasGasUserTest() {
+        Figure3T[][] table = {
+                {new Figure3T(true), new Figure3T(), new Figure3T()},
+                {new Figure3T(false), new Figure3T(true), new Figure3T()},
+                {new Figure3T(false), new Figure3T(), new Figure3T(true)},
+        };
+        Logic3T login = new Logic3T(table);
+        assertThat(login.hasGap(), is(true));
+    }
 }
